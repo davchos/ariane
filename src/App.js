eb3917644746bf52a8206f56d3820dd57d24adf1
davchos <dchosrova@gmail.com>
2021-07-22 07:41:19 +0200

    Add header and footer

--- src/App.js
+++ src/App.js
@@ -11,15 +11,29 @@ function App() {
   const switchSate = status1 && status2 && status3 ? true : false;
 
   return (
-    <div className="main">
-      <div>
-        <Switch status={status1} setStatus={setStatus1} />
-        <Switch status={status2} setStatus={setStatus2} />
-        <Switch status={status3} setStatus={setStatus3} />
+    <div>
+      <header className="header">
+        <div>
+          <span style={{ color: "blue" }}>
+            <i className="fas fa-calculator"></i>
+          </span>
+          <span style={{ fontWeight: "bold" }}>React Counter</span>
+        </div>
+      </header>
+      <div className="main">
+        <div>
+          <Switch status={status1} setStatus={setStatus1} />
+          <Switch status={status2} setStatus={setStatus2} />
+          <Switch status={status3} setStatus={setStatus3} />
+        </div>
+        <div className="flag">
+          <Flag color={switchSate ? "flag-green" : "flag-red"} />
+        </div>
       </div>
-      <div className="flag">
-        <Flag color={switchSate ? "flag-green" : "flag-red"} />
-      </div>
+      <footer className="footer">
+        Made with <span style={{ fontWeight: "bold" }}>React</span> at{" "}
+        <span style={{ fontWeight: "bold" }}>Le Reacteur</span> by david
+      </footer>
     </div>
   );
 }
